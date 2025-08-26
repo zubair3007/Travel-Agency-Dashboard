bda7a93d2157c10f872031028af45f0adda5315a
Zubair Khan <kzubair129@gmail.com>
2025-08-26 22:25:43 +0530

    modification in setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
